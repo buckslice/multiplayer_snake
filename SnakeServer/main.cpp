f510ec368fd1ba8c1e5b62d3c4bcd3ee1e27bc20
buckslice <buckslice@gmail.com>
2016-02-09 19:10:33 -0800

    pushing empty server project and font

---
+++ SnakeServer/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+
+
+
+int main() {
+    int port;
+
+    std::cout << "Enter port: ";
+    std::cin >> port;
+
+    /* set event handler */
+    //server.setOpenHandler(openHandler);
+    //server.setCloseHandler(closeHandler);
+    //server.setMessageHandler(messageHandler);
+    //server.setPeriodicHandler(periodicHandler);
+
+    /* start the chatroom server, listen to ip '127.0.0.1' and port '8000' */
+    //server.startServer(port);
+
+
+    return 0;
+}
